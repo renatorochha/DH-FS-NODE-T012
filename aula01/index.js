d9ce64ca9f7e92fb18b784f95ce5373aa41b4f9c
renatorochha <renators65x@gmail.com>
2020-10-19 19:40:55 -0300

    adicionando a aula01

---
+++ aula01/index.js
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+// Temas e plugins para usar no VS code
+//Fira Code
+//Material Icon Theme
+//Dracula
+// CTRL + shift + x == File > preferences > Extensions
+
+// Fira code no VS Code: pesquisar na internet como ativar
+
+//comandos
+
+//chamar um arquivo para executar no terminal bash
+//Sempre usar: node aula04/index.js
+//de acordo com a pasta onde esta o arquivo.
+
+
+
