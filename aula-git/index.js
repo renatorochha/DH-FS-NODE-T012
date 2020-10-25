f300e1dbfdfa3cee272aa3099f221bb76e0553e6
renatorochha <renators65x@gmail.com>
2020-10-25 11:56:40 -0300

    adicionando resumo da aula git e branch

---
+++ aula-git/index.js
@@ -0,0 +1,31 @@
+/*
+Resumo da Aula de Git hub 14/10/20
+
+Na aula de hoje abordamos sobre: 
+- Conflitos;
+- Criação de branch;
+- Como trocar para uma branch;
+- Como trocar para branch já existente;
+- Criação de pull request;
+- Como adicionar um reviewer (para revisar nosso código que estamos mexendo em determinada branch);
+
+RESUMO DE COMANDOS UTILIZADOS
+- git add (adicionar no stage);
+
+- git commit -m "menssagem" (adicionar mensagem para pacote de alterações)
+
+- git pull (traz alterações do repositório remoto para nosso repositorio local, ou, busca alinhar as alterações
+da nossa máquina);
+
+- git push (envia as alterções que estão no stage para o github efetivamente)
+
+COMANDOS PARA TRABALHAR COM BRANCH
+
+- git checkout (alteração da branch atual para uma outra branch existente)
+
+- git checkout -b nomeDaBranch (efetua a criação de uma branch, e acaba "clonando" a versão 
+(todos arquivos do repositório), com base na branch que estava no momento em que criou a branch nova, ou seja,
+se voçê criar uma branch x a partir da master a branch x terá todos arquivos que a branch master tinha até
+aquele momento).
+
+*/
